4c7bac8805989d585387c1764b751a22d1e8eccf
Jonathan Ware <jonathan.ware02@gmail.com>
2023-08-17 10:12:27 -0500

    acquire py updated

--- acquire.py
+++ acquire.py
@@ -3,100 +3,104 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-def scrape_and_save_articles(urls, csv_filename):
-   
-    if os.path.exists(csv_filename):
-        df = pd.read_csv(csv_filename)
-    else:
-        df = pd.DataFrame(columns=['title', 'content'])
-    
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+"""
+A module for obtaining repo readme and language data from the github API.
+Before using this module, read through it, and follow the instructions marked
+TODO.
+After doing so, run it like this:
+    python acquire.py
+To create the `data.json` file that contains the data.
+"""
+import os
+import json
+from typing import Dict, List, Optional, Union, cast
+import requests
+
+from env import github_token, github_username
+
+# TODO: Make a github personal access token.
+#     1. Go here and generate a personal access token https://github.com/settings/tokens
+#        You do _not_ need select any scopes, i.e. leave all the checkboxes unchecked
+#     2. Save it in your env.py file under the variable `github_token`
+# TODO: Add your github username to your env.py file under the variable `github_username`
+# TODO: Add more repositories to the `REPOS` list below.
+
+REPOS = []
+ 
+
+headers = {"Authorization": f"token {github_token}", "User-Agent": github_username}
+
+if headers["Authorization"] == "token " or headers["User-Agent"] == "":
+    raise Exception(
+        "You need to follow the instructions marked TODO in this script before trying to use it"
+    )
+
+
+def github_api_request(url: str) -> Union[List, Dict]:
+    response = requests.get(url, headers=headers)
+    response_data = response.json()
+    if response.status_code != 200:
+        raise Exception(
+            f"Error response from github api! status code: {response.status_code}, "
+            f"response: {json.dumps(response_data)}"
+        )
+    return response_data
+
+
+def get_repo_language(repo: str) -> str:
+    url = f"https://api.github.com/repos/{repo}"
+    repo_info = github_api_request(url)
+    if type(repo_info) is dict:
+        repo_info = cast(Dict, repo_info)
+        return repo_info.get("language", None)
+    raise Exception(
+        f"Expecting a dictionary response from {url}, instead got {json.dumps(repo_info)}"
+    )
+
+
+def get_repo_contents(repo: str) -> List[Dict[str, str]]:
+    url = f"https://api.github.com/repos/{repo}/contents/"
+    contents = github_api_request(url)
+    if type(contents) is list:
+        contents = cast(List, contents)
+        return contents
+    raise Exception(
+        f"Expecting a list response from {url}, instead got {json.dumps(contents)}"
+    )
+
+
+def get_readme_download_url(files: List[Dict[str, str]]) -> str:
+    """
+    Takes in a response from the github api that lists the files in a repo and
+    returns the url that can be used to download the repo's README file.
+    """
+    for file in files:
+        if file["name"].lower().startswith("readme"):
+            return file["download_url"]
+    return ""
+
+
+def process_repo(repo: str) -> Dict[str, str]:
+    """
+    Takes a repo name like "gocodeup/codeup-setup-script" and returns a
+    dictionary with the language of the repo and the readme contents.
+    """
+    contents = get_repo_contents(repo)
+    readme_contents = requests.get(get_readme_download_url(contents)).text
+    return {
+        "repo": repo,
+        "language": get_repo_language(repo),
+        "readme_contents": readme_contents,
     }
-    
-    for url in urls:
-        response = requests.get(url, headers=headers)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        
-        title = soup.find('h1').text.strip()
-        content = ' '.join([p.text.strip() for p in soup.find_all('p')])
-        
-        article = {
-            'title': title,
-            'content': content
-        }
-        
-        
-        df = df.append(article, ignore_index=True)
-    df.to_csv(csv_filename, index=False)
-    print(f"Articles saved to '{csv_filename}'")
-    
-    return df
 
 
+def scrape_github_data() -> List[Dict[str, str]]:
+    """
+    Loop through all of the repos and process them. Returns the processed data.
+    """
+    return [process_repo(repo) for repo in REPOS]
 
 
-CSV_FILENAME = "inshorts_articles.csv"
-
-def scrape_inshorts_articles(urls):
-    articles = []
-
-    for url in urls:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-
-        article_cards= soup.find_all(itemtype='http://schema.org/NewsArticle')
-
-        for card in article_cards:
-            headline = card.find('span', itemprop='headline').text
-            summary = card.find('div', itemprop='articleBody').text
-            author = card.find('span', class_ = 'author').text
-            date = card.find('span', itemprop = 'datePublished').text
-            day = card.find('span', clas='date').text
-            
-            category = url.split('/')[-1]
-            
-            
-            articles.append({
-                'headline': headline,
-                'summary': summary,
-                'author': author,
-                'time': date,
-                'day': day,
-                'category': category
-                
-            })
-
-    df = pd.DataFrame(articles)
-    return df
-
-def get_inshorts_data():
-    if os.path.exists(CSV_FILENAME):
-        print("Loading data from CSV...")
-        return pd.read_csv(CSV_FILENAME)
-    else:
-        base_url = "https://inshorts.com/en/read/"
-        pages = [
-            "business",
-            "sports",
-            "technology",
-            "entertainment",
-            "india",
-            "politics",
-            "startups",
-            "hatke",
-            "international",
-            "automobile",
-            "science",
-            "travel",
-            "miscellaneous",
-            "fashion",
-            "education",    
-        ]      
-
-        urls = [base_url + page for page in pages]
-        result_df = scrape_inshorts_articles(urls)
-        result_df.to_csv(CSV_FILENAME, index=False)
-        print("Data scraped and saved to CSV.")
-        return result_df
-
+if __name__ == "__main__":
+    data = scrape_github_data()
+    json.dump(data, open("data2.json", "w"), indent=1)
