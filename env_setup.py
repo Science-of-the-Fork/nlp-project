bdc676a678a5bc8f920df532a70ee5aebcba0638
MigashaneVictoire <Migashanevictoire@gmail.com>
2023-08-17 15:28:13 -0500

    add aquire

---
+++ env_setup.py
@@ -0,0 +1,9 @@
+# TODO: Make a github personal access token.
+#     1. Go here and generate a personal access token https://github.com/settings/tokens
+#        You do _not_ need select any scopes, i.e. leave all the checkboxes unchecked
+#     2. Save it in your env.py file under the variable `github_token`
+# TODO: Add your github username to your env.py file under the variable `github_username`
+# TODO: Add more repositories to the `REPOS` list below.
+
+github_token = ""
+github_username = ""
